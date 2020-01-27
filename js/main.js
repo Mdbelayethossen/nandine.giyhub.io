120ae88b81e3dc93ad0a84e4593521a911657362
Mdbelayethossen <59694089+Mdbelayethossen@users.noreply.github.com>
2020-01-27 17:38:06 +0600

    owl init
    removed junk

--- js/main.js
+++ js/main.js
@@ -1,6 +1,38 @@
 $(document).ready(function(){
-    $('.counter').counterUp({
-        delay: 10,
-        time: 1000
-    });
+    let $btns = $('.portpolio-area  .button-group button');
+
+
+    $btns.click(function (e) {
+
+        $('.portpolio-area .button-group button').removeClass('active');
+        e.target.classList.add('active');
+
+        let selector = $(e.target).attr('data-filter');
+        $('.portpolio-area .grid').isotope({
+            filter: selector
+        });
+
+        return false;
+    })
+
+    $('.portpolio-area .button-group #btn1').trigger('click');
+
+    // Owl-carousel
+
+    $(' .my-team .owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: true,
+        dots: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            560: {
+                items: 2
+            },
+            1200: {
+                items: 4
+            }
+        }
+    })
   });
