ef69018974939894330afc7f7f43dec7994d5b31
Mdbelayethossen <59694089+Mdbelayethossen@users.noreply.github.com>
2020-01-28 09:52:28 +0600

    counter up

--- js/main.js
+++ js/main.js
@@ -35,4 +35,27 @@ $(document).ready(function(){
             }
         }
     })
+    jQuery(function ($) {
+    	"use strict";
+    
+    	var counterUp = window.counterUp["default"]; // import counterUp from "counterup2"
+    
+    	var $counters = $("count-area .counter");
+    
+    	/* Start counting, do this on DOM ready or with Waypoints. */
+		$counters.each(function (ignore, counter) {
+			var waypoint = new Waypoint( {
+				element: $(this),
+				handler: function() { 
+					counterUp(counter, {
+						duration: 5000,
+						delay: 16
+					}); 
+					this.destroy();
+				},
+				offset: 'bottom-in-view',
+			} );
+		});
+
+	});
   });
